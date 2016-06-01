d702b878ab2bbeb07f47ca52629c9ea5860c5078
jimmyPM <pardomendozajimmy@gmail.com>
2016-06-01 07:29:44 -0400

    Add files via upload

---
+++ proDev/public/templates.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+	<title>text</title>
+</head>
+<body>
+<h1>Hola Bienbenido  <?php echo $nombre ?>, tu edad es: <?php echo $edad ?>;</h1>
+</body>
+</html>
